73b3d7273a82479a9e7824f687be30f496d7f09c
Axel Jacquot <axel.jacquot@outlook.com>
2018-12-03 22:02:05 +0100

    Update main.c

--- PWM_TP/src/main.c
+++ PWM_TP/src/main.c
@@ -5,12 +5,12 @@ void wait (unsigned int time);
 TIM_HandleTypeDef TIM_Handle;		//Variable contenant la configuration du timer 4(mis dans chaque fonction à cause de cela).
 
 int main(void){
-	GPIO_Configuration(GPIOD, GPIO_MODE_AF_PP, Green | Blue, GPIO_AF2_TIM4);		//Configuration des pins des Leds verte et bleu se trouvant sur le port D en mode alternatif PP, les pins prendront leurs états des sorties du timer 4
+	GPIO_Configuration(GPIOD, GPIO_MODE_AF_PP, Green | Blue, GPIO_AF2_TIM4);	//Configuration des pins des Leds verte et bleu se trouvant sur le port D en mode alternatif PP, les pins prendront leurs états des sorties du timer 4
 	Timer_Config(&TIM_Handle,TIM4, 16000, 2000);												//Activation et Configuration du timer 4 à la fréquence souhaitée
-	Timer_Mode_OCPWM_Config(&TIM_Handle, TIM_OCMODE_PWM1, TIM_OCPOLARITY_HIGH);					//Configuration du mode du Timer 4
-	Timer_PWM_OCPWM_Pulse_Channel(&TIM_Handle, TIM_CHANNEL_1, 500);								//Réglage du temps d'état bas sur le channel 1 du timer 4
+	Timer_Mode_OCPWM_Config(&TIM_Handle, TIM_OCMODE_PWM1, TIM_OCPOLARITY_HIGH);	//Configuration du mode du Timer 4
+	Timer_PWM_OCPWM_Pulse_Channel(&TIM_Handle, TIM_CHANNEL_1, 500);			//Réglage du temps d'état bas sur le channel 1 du timer 4
 	Timer_PWM_OCPWM_Pulse_Channel(&TIM_Handle, TIM_CHANNEL_4, 1500);
-	HAL_TIM_OC_Start(&TIM_Handle, TIM_CHANNEL_1);												//Activation du channel 1 du timer 4
+	HAL_TIM_OC_Start(&TIM_Handle, TIM_CHANNEL_1);					//Activation du channel 1 du timer 4
 	HAL_TIM_OC_Start(&TIM_Handle, TIM_CHANNEL_4);
 	for(;;){
 
