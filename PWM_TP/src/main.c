7ba6997fc05811563d13fae1d932d4229643848a
AxelJacquot <axel.jacquot@outlook.com>
2018-12-03 21:16:52 +0100

    Modified the percent of PWM

--- PWM_TP/src/main.c
+++ PWM_TP/src/main.c
@@ -8,7 +8,7 @@ int main(void){
 	GPIO_Configuration(GPIOD, GPIO_MODE_AF_PP, Green | Orange | Red | Blue, GPIO_AF2_TIM4);
 	Timer_Config(&TIM_Handle,TIM4, 16000, 2000);
 	Timer_Mode_OCPWM_Config(&TIM_Handle, TIM_OCMODE_PWM2, TIM_OCPOLARITY_HIGH);
-	Timer_PWM_OCPWM_Pulse_Channel(&TIM_Handle, TIM_CHANNEL_1, 250);
+	Timer_PWM_OCPWM_Pulse_Channel(&TIM_Handle, TIM_CHANNEL_1, 500);
 	Timer_PWM_OCPWM_Pulse_Channel(&TIM_Handle, TIM_CHANNEL_4, 1500);
 	HAL_TIM_OC_Start(&TIM_Handle, TIM_CHANNEL_1);
 	HAL_TIM_OC_Start(&TIM_Handle, TIM_CHANNEL_4);
